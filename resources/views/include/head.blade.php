a66815f5e0111a393011e02ea6a9785ce77fbdaf
sunnexy <sogbonnapaschal@gmail.com>
2022-02-20 21:26:49 +0100

    updatingcxsvdk

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -2,7 +2,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>PayPhast Money Transfer</title>
+    <title>Hyilux - Investment Business HTML Template</title>
 
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
